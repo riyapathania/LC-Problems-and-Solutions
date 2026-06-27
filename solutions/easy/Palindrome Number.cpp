d9641aa3aee306b5d8a7c2c8bb0ac2bff04be150
Harshitha Pathania <154575279+riyapathania@users.noreply.github.com>
2026-06-27 18:32:00 -0400

    Add solution for Palindrome Number

---
+++ solutions/easy/Palindrome Number.cpp
@@ -0,0 +1,13 @@
+// Title: Palindrome Number
+            // Difficulty: Easy
+            // Language: C++
+            // Link: https://leetcode.com/problems/palindrome-number/
+
+        }
+            right--;
+            left++;
+                return false;
+            }
+            if (number[left] != number[right]){
+        while (left < right) {
+        int right = number.size() - 1;
